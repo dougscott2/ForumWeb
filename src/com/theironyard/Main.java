009fc8f205440a9abca9f83b10e782755d6762cf
DrScott <dougscott2@gmail.com>
2015-10-29 12:12:21 -0400

    added create-message post

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -33,6 +33,7 @@ public class Main {
                     HashMap m = new HashMap();
                     m.put("threads", threads);
                     m.put("username", username);
+                    m.put("replyId", -1);
 
                     return new ModelAndView(m, "threads.html");
                 }),
@@ -42,13 +43,18 @@ public class Main {
         Spark.get(
                 "/replies",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     HashMap m = new HashMap();
+                    m.put("username", username);
 
                     String id = request.queryParams("id");
                     try {
                         int idNum = Integer.valueOf(id);
                         Message message = messages.get(idNum);
                         m.put("message", message);
+                        m.put("replyId", message.id);
 
                         ArrayList<Message> replies = new ArrayList();
                         for (Message msg : messages){
@@ -91,6 +97,27 @@ public class Main {
 
                     Session session = request.session();
                     session.attribute("username", username);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.post(
+                "/create-message",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    if (username==null){
+                        Spark.halt(403);
+                    }
+                    String replyId = request.queryParams("replyId");
+                    String text = request.queryParams("text");
+                    try{
+                        int replyIdNum = Integer.valueOf(replyId);
+                        Message message = new Message(messages.size(), replyIdNum, username, text);
+                        messages.add(message);
+                    }catch (Exception e){}
 
                     response.redirect("/");
                     return "";
